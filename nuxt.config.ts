3fe33788649523ed5e24999dbfe14aa04ff6b74d
anthonylorendeaux <anthony.lorendeaux@icloud.com>
2026-01-21 17:39:56 +0100

    Nitro update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,30 @@
 export default defineNuxtConfig({
+  nitro: {
+    preset: "cloudflare_pages",
+    cloudflare: {
+      deployConfig: true,
+      nodeCompat: true
+    },
+    prerender: {
+      crawlLinks: true,
+      routes: ['/'],
+    },
+  },
   modules: [
     '@nuxt/ui',
     '@nuxt/content',
     '@nuxt/image',
-/*     'nuxt-studio',
- */  ],
+    'nuxt-studio',
+  ],
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   compatibilityDate: '2024-04-03',
-  /*   studio: {
-      repository: {
-        provider: 'github',
-        owner: 'anthonylorendeaux',
-        repo: 'recette-app',
-        branch: 'master'
-      }
-    } */
+  studio: {
+    repository: {
+      provider: 'github',
+      owner: 'anthonylorendeaux',
+      repo: 'recette-app',
+      branch: 'master'
+    }
+  }
 })
